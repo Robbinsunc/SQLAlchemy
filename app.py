c13518330dfdec6b7188da0f1fbc425c2d536dcb
Robbinsunc <robbinsunc@gmail.com>
2019-02-09 21:32:35 -0500

    Final Commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, MetaData, Column, String, Table
 
 from flask import Flask, jsonify
 
@@ -15,18 +15,18 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-Measurement = Base.classes.Measurement
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
+
 
 #################################################
 # Flask Setup
@@ -55,14 +55,15 @@ def precipitation():
     """Convert the query results to a Dictionary using date as the key and prcp as the value."""
     results = session.query(Measurement.date, Measurement.prcp).\
         group_by(Measurement.date).all()
+    session.commit()
     
     return jsonify(results)
 
 @app.route("/api/v1.0/stations")
 def stations():
     """Return a JSON list of stations from the dataset."""
-    results = session.query(Measurement.stations).\
-        group_by(Measurement.stations).all()
+    results = session.query(Measurement.station).\
+        group_by(Measurement.station).all()
 
     return jsonify(results)
 
@@ -76,7 +77,10 @@ def tobs():
         filter(Measurement.date <= date).\
         order_by(Measurement.date).all()
 
-    return jsonify(results)
+    # Convert list of tuples into normal list
+    all_tobs = list(np.ravel(results))
+
+    return jsonify(all_tobs)
 
 
 @app.route("/api/v1.0/<start>")
@@ -86,7 +90,10 @@ def temp_data(start_date):
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start_date).all()
 
-    return jsonify(results)
+    # Convert list of tuples into normal list
+    all_date = list(np.ravel(results))
+
+    return jsonify(all_date)
 
 @app.route("/api/v1.0/<start>/<end>")
 def temp_range(start_date, end_date):
@@ -95,7 +102,10 @@ def temp_range(start_date, end_date):
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
 
-    return jsonify(results)
+    # Convert list of tuples into normal list
+    startend = list(np.ravel(results))
+
+    return jsonify(startend)
 
 if __name__ == "__main__":
     app.run(debug=True)
